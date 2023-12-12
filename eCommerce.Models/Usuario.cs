511f3c5cc19427d2a98d9cced2c25105180cb4bb
Yago Escalfoni <yago@otimagestao.com>
2023-12-12 12:39:57 -0300

    Adicionar arquivos de projeto.

---
+++ eCommerce.Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace eCommerce.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }   
+        public string? Email { get; set; }
+        public string? Sexo { get; set; }
+        public string? RG { get; set; }
+        public string? CPF { get; set; }
+        public string? NomeMae { get; set; }
+        public string? SituacaoCadastro { get; set; }
+        public DateTimeOffset DataCadastro { get; set; }
+        public Contato? Contato { get; set; }        
+        public ICollection<EnderecoEntrega> EnderecoEntrega { get; set; }
+        
+        /*
+        / TODO - Vincular com as classes:
+        / - Cliente
+        / - EnderecoEntrega
+        / - Departamento
+        */
+
+    }
+}
