89198eb1b25525598d0b2e82846e566a73013969
Yago Escalfoni <yago@otimagestao.com>
2023-12-15 08:19:19 -0300

    feature: fluentAPI

---
+++ eCommerce.Models.FluetAPI/Models/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace eCommerce.Models
+{
+    /*
+     * EFCore + Migration:
+     * Tabela: Usuarios
+     * Id - PK
+     * Nome - nvarchar - not null
+     * Email - nvarchar - not null
+     * Sexo - nvarchar - null
+     * CPF - nvarchar - not null
+     */
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Sexo { get; set; }
+        public string? RG { get; set; }
+        public string CPF { get; set; } = null!;
+        public string? NomeMae { get; set; }
+        public string? NomePai { get; set; }
+        public string? SituacaoCadastro { get; set; }
+        public DateTimeOffset DataCadastro { get; set; }
+        public Contato? Contato { get; set; }        
+        public ICollection<EnderecoEntrega>? EnderecoEntrega { get; set; }
+        public ICollection<Departamento>? Departamentos { get; set; }
+        
+        /*
+        / TODO - Vincular com as classes:
+        / - Cliente
+        / - EnderecoEntrega
+        / - Departamento
+        */
+
+    }
+}
---
+++ eCommerce.Models.FluetAPI/eCommerceFluentContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models.FluetAPI
+{
+    internal class eCommerceFluentContext : DbContext
+    {
+        /*
+         * Conexão sem dinstinção de ambientes de execução
+         */
+
+        public eCommerceFluentContext(DbContextOptions<eCommerceFluentContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario>? Usuarios { get; set; }
+        public DbSet<Contato>? Contatos { get; set; }
+        public DbSet<EnderecoEntrega>? EnderecosEntrega { get; set; }
+        public DbSet<Departamento>? Departamentos { get; set; }
+    
+    }
+}
